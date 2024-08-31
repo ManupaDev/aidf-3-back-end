902192ca6aa994a1553da15623ff62b8023f7d03
ManupaDev <manuapdevsuite@gmail.com>
2024-08-31 22:56:28 +0530

    feat: session-5

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import jobsRouter from "./api/jobs.js";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api/jobs", jobsRouter);
+
 const PORT = 8000;
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Webservice is listening on port ${PORT}`);
 });
